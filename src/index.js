71e41130272ebafd44de15dbeefbe116be6cf089
rafaellima1412 <rafaellima1412@gmail.com>
2021-07-21 14:26:19 -0400

    configurando

--- src/index.js
+++ src/index.js
@@ -21,115 +21,132 @@ Delete -> Remover um item específico
 // O meu será de Raças de Cachorro
 
 const lista = [
-    {
-        id: 1,
-        nome: "Pinscher",
-        imagemUrl: "https://pbs.twimg.com/media/Da3FmbSWkAEuwUJ.jpg",
-    },
-    {
-        id: 2,
-        nome: "Golden Retriever",
-        imagemUrl:
-            "https://f.i.uol.com.br/fotografia/2017/10/27/150912457859f369e28eaa5_1509124578_3x2_md.jpg",
-    },
-    {
-        id: 3,
-        nome: "Labrador",
-        imagemUrl:
-            "https://www.clubeparacachorros.com.br/wp-content/uploads/2014/07/labrador-amarelo.jpg",
-    },
-    {
-        id: 4,
-        nome: "Yorkshire",
-        imagemUrl:
-            "https://www.petlove.com.br/images/breeds/192471/profile/original/yorkshire-p.jpg?1532539683",
-    },
-    {
-        id: 5,
-        nome: "Husky Siberiano",
-        imagemUrl:
-            "https://www.petlove.com.br/images/breeds/193082/profile/original/husky_siberian-p.jpg?1532539123",
-    },
+  {
+    id: 1,
+    nome: "Pinscher",
+    imagemUrl: "https://pbs.twimg.com/media/Da3FmbSWkAEuwUJ.jpg",
+  },
+  {
+    id: 2,
+    nome: "Golden Retriever",
+    imagemUrl:
+      "https://f.i.uol.com.br/fotografia/2017/10/27/150912457859f369e28eaa5_1509124578_3x2_md.jpg",
+  },
+  {
+    id: 3,
+    nome: "Labrador",
+    imagemUrl:
+      "https://www.clubeparacachorros.com.br/wp-content/uploads/2014/07/labrador-amarelo.jpg",
+  },
+  {
+    id: 4,
+    nome: "Yorkshire",
+    imagemUrl:
+      "https://www.petlove.com.br/images/breeds/192471/profile/original/yorkshire-p.jpg?1532539683",
+  },
+  {
+    id: 5,
+    nome: "Husky Siberiano",
+    imagemUrl:
+      "https://www.petlove.com.br/images/breeds/193082/profile/original/husky_siberian-p.jpg?1532539123",
+  },
 ];
-
+// aqui faço endpoint diferente para nao trazer tudo
 function Item(props) {
-    const indice = props.indice;
+  const indice = props.indice;
 
-    const item = lista[indice];
+  const item = lista[indice];
 
-    return (
-        <a href={"/visualizar/" + indice}>
-            <div className="item">
-                <h1 className="item__title">{item.nome}</h1>
-                <img src={item.imagemUrl} alt={item.nome} width="200" />
-            </div>
-        </a>
-    );
+  return (
+    <a href={"/visualizar/" + indice}>
+      <div className="item">
+        <h1 className="item__title">{item.nome}</h1>
+        <img src={item.imagemUrl} alt={item.nome} width="200" />
+      </div>
+    </a>
+  );
 }
-
+// aqui trago tudo
 function Lista() {
-    return (
-        <div className="lista">
-            {lista.map((item, index) => (
-                <Item indice={index} key={index} />
-            ))}
-        </div>
-    );
+  console.log("lista caregada");
+  //integrando com backend
+  //como e um promisse tem que coloca ro wait
+  const obterresultado = async () => {
+    console.log("obter resultado");
+    const resultado = await fetch("https://backend-flexivel.herokuapp.com", {
+      headers: new Headers({
+        //que foi definido no headers
+        Authorization: "rafaellima1412@gmail.com",
+      }),
+    });
+  };
+  console.log(resultado);
+  const dados = await resultado.json();
+  console.log(dados);
+
+  //chamamos a função para rodar
+  obterresultado();
+  return (
+    <div className="lista">
+      {lista.map((item, index) => (
+        <Item indice={index} key={index} />
+      ))}
+    </div>
+  );
 }
 
 function Header() {
-    return (
-        <header className="header">
-            <a href="/">
-                <img
-                    src="https://www.oceanbrasil.com/img/general/logoOceanI.png"
-                    alt="Samsung Ocean"
-                    width="300"
-                />
-            </a>
-        </header>
-    );
+  return (
+    <header className="header">
+      <a href="/">
+        <img
+          src="https://www.oceanbrasil.com/img/general/logoOceanI.png"
+          alt="Samsung Ocean"
+          width="300"
+        />
+      </a>
+    </header>
+  );
 }
 
 function Footer() {
-    return <footer className="footer">Todos os direitos reservados.</footer>;
+  return <footer className="footer">Todos os direitos reservados.</footer>;
 }
 
 function ListarItens() {
-    return (
-        <div>
-            <Lista />
-        </div>
-    );
+  return (
+    <div>
+      <Lista />
+    </div>
+  );
 }
 
 function VisualizarItem(props) {
-    return (
-        <div>
-            <Item indice={props.match.params.id} />
-        </div>
-    );
+  return (
+    <div>
+      <Item indice={props.match.params.id} />
+    </div>
+  );
 }
 
 function App() {
-    return (
-        <div className="app">
-            <Header />
-            <Switch>
-                <Route path="/" exact={true} component={ListarItens} />
-
-                <Route path="/visualizar/:id" component={VisualizarItem} />
-            </Switch>
-            <Footer />
-        </div>
-    );
+  return (
+    <div className="app">
+      <Header />
+      <Switch>
+        <Route path="/" exact={true} component={ListarItens} />
+        <Route path="/visualizar/:id" component={VisualizarItem} />
+      </Switch>
+      <Footer />
+    </div>
+  );
 }
 
 ReactDOM.render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </React.StrictMode>,
-    document.getElementById("root")
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
